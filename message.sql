00606f42953179da62c4f3fbdad4a3fa862527f6
hallek7 <kebe0010@umn.edu>
2018-10-09 19:52:19 -0500

    1st push

---
+++ message.sql
@@ -0,0 +1,12 @@
+
+DROP DATABASE IF EXISTS messages_db;
+CREATE DATABASE messages_db;
+USE messages_db; 
+create table message ( 
+ when INTEGER (10),
+ Description  varchar(250) NULL, 
+ user name (250) NULL, 
+ PRIMARY KEY (id)
+
+); 
+
